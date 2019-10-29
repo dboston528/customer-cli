c0401bf7c5abc308d65cc7313432042be42f660e
DeAndre Boston <dboston3@yahoo.com>
2019-10-29 16:08:04 -0500

    Initial commit

---
+++ commands.js
@@ -0,0 +1,51 @@
+const program = require('commander');
+const { prompt } = require('inquirer');
+const { addCustomer, findCustomer } = require('./index');
+
+program.version('1.0.0').description('Client Management System');
+
+//Customer Questions
+const questions = [
+  {
+    type: 'input',
+    name: 'firstname',
+    message: 'Customer First Name'
+  },
+  {
+    type: 'input',
+    name: 'lastname',
+    message: 'Customer Last Name'
+  },
+  {
+    type: 'input',
+    name: 'phone',
+    message: 'Customer Phone Number'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Customer Email Address'
+  }
+];
+// program
+//   .command('add <firstname> <lastname> <phone> <email>')
+//   .alias('a')
+//   .description('Add a customer')
+//   .action((firstname, lastname, phone, email) => {
+//     addCustomer({ firstname, lastname, phone, email });
+//   });
+
+program
+  .command('add')
+  .alias('a')
+  .description('Add a customer')
+  .action(() => {
+    prompt(questions).then(answers => addCustomer(answers));
+  });
+
+program
+  .command('find <name>')
+  .alias('f')
+  .description('Find a customer')
+  .action(name => findCustomer(name));
+program.parse(process.argv);
---
+++ models/customer.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const customerSchema = mongoose.Schema({
+  firstname: { type: String },
+  lastname: { type: String },
+  phone: { type: String },
+  email: { type: String }
+});
+
+//Define and export
+module.exports = mongoose.model('Customer', customerSchema);
